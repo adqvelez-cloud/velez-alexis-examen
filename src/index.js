39ded295d8997b3356c2b3caebf6ef06e62250ce
alexis <adq.velez@yavirac.edu.ec>
2025-11-25 15:06:03 -0500

    Fix path to public directory using process.cwd()

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Servir archivos estáticos desde el directorio 'public'
-app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(express.static(path.join(process.cwd(), 'public')));
 
 function sumar(a, b) {
   return a + b;
@@ -13,7 +13,7 @@ function sumar(a, b) {
 module.exports = { sumar };
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+  res.sendFile(path.join(process.cwd(), 'public', 'index.html'));
 });
 
 // Solo iniciar el servidor si este archivo se ejecuta directamente
