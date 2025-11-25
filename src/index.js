2d97b8383b87df0dc7632a9878c7667d89ea5cbe
alexis <adq.velez@yavirac.edu.ec>
2025-11-25 15:21:18 -0500

    cambios nuevos 2

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ app.get("/", (req, res) => {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Quinche - App Desplegada</title>
+    <title>Alexis verificando los cambios, que si vale</title>
     <style>
         * {
             margin: 0;
